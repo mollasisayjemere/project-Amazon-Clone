a9605a0fcdc2cb7b03d102f3514964372bb8c699
mollasisayjemere <mollasisayjemere@gmail.com>
2025-03-13 14:52:06 +0100

    Footer correction

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,6 +4,7 @@ import Layout from "../../Components/LayOut/Layout.jsx";
 import Carousell from "../../Components/Carousell/Carousell.jsx";
 import Category from "../../Components/Category/Category.jsx";
 import Product from "../../Components/Products/Product.jsx";
+import Footer from "../../Components/Footer/Footer.jsx";
 
 function LandingPage() {
   return (
@@ -11,6 +12,7 @@ function LandingPage() {
       <Carousell />
       <Category />
       <Product />
+      <Footer/>
     </Layout>
   );
 }
